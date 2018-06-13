c8a60c2890987452768391fd9236fa22c0dda4e5
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2018-06-13 17:15:47 -0300

    Finish page promotion

--- includes/promotion.php
+++ includes/promotion.php
@@ -1,12 +1,12 @@
 <div class="promotion">
     <section class="promotion__banner--outline">
         <div class="promotion__banner--base">
-            <img class="promotion__banner--image-tree" src="" alt="">
+            <img class="promotion__banner--image-tree" src="assets/img/coqueiros.png" alt="">
             <div class="promotion__banner--title">
                 <h1>Promoções</h1>
                 <h2>Fique por dentro de nossas promoções e descontos</h2>
             </div>
-            <img class="promotion__banner--image-sun" src="" alt="">
+            <img class="promotion__banner--image-sun" src="assets/img/gifs/sun.gif" alt="">
         </div>
     </section>
 
@@ -16,9 +16,7 @@
         <h3 class="promotion__one--title">Setembro</h3>
         <h2 class="promotion__one--sutitle">Inverno</h2>
         <figure class="promotion__one--body">
-            <div class="promotion__one--image-outline">
-                <img class="promotion__one--image" src="" alt="">
-            </div>
+            <img class="promotion__one--image" src="assets/img/inverno2.png" alt="">
             <div class="promotion__one--description">
                 <figcaption>           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla quam velit, vulputate eu pharetra nec, mattis ac neque. Duis vulputate commodo lectus, ac blandit elit tincidunt id. Sed rhoncus, tortor sed eleifend tristique, tortor mauris molestie elit, et lacinia ipsum quam nec dui. 
                  Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.</figcaption>        
@@ -46,9 +44,7 @@
              Quisque nec mauris sit amet elit iaculis pretium sit amet quis magna. Aenean velit odio, elementum in tempus ut, vehicula eu diam. Pellentesque rhoncus aliquam mattis. Ut vulputate eros sed felis sodales nec vulputate justo hendrerit. Vivamus varius pretium ligula, a aliquam odio euismod sit amet. Quisque laoreet sem sit amet orci ullamcorper at ultricies metus viverra. Pellentesque arcu mauris, malesuada quis ornare accumsan, blandit sed diam.</figcaption>        
                 <input type="submit" value="PARTICIPE">
             </div>
-             <div class="promotion__two--image-outline">
-                <img class="promotion__two--image" src="" alt="">
-            </div>       
+            <img class="promotion__two--image" src="assets/img/children-pool-party3.png" alt="">   
         </figure>
     </section>
 
