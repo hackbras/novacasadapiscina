e0923ee5e15b58a6f8c1b76c9d8b00b9cf7afecb
vm-w7x64ult-PC\vm-w7x64ult <hackbras@outlook.com>
2018-06-16 11:45:03 -0300

    Page contact styles

--- includes/contact.php
+++ includes/contact.php
@@ -2,9 +2,9 @@
 <div class="contact">
     <section class="contact__item contact__email">
         <form class="contact__item contact__email--form">
-            <input type="text" placeholder="Nome:">
-            <input type="text" placeholder="Email:">
-             <textarea >   </textarea>
+            <input class="text__green" type="text" placeholder="Nome:">
+            <input class="text__green" type="text" placeholder="Email:">
+            <textarea  class="text__green" placeholder="Escreva sua mensagem aqui." rows="10">   </textarea>
             <input class="button-white" type="submit" value="Enviar">
         </form>
     </section>
@@ -30,7 +30,7 @@
 
     <section class="contact__item contact__map">
         <div class="contact__item contact__map--elements">
-            <iframe></iframe>
+            <iframe class="contact__map--image" width="400" height="250" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3748.7265572504834!2d-44.0717529850841!3d-20.019987986553396!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xa6bf32d14bd639%3A0xff543d3f94a513a9!2sAv.+S%C3%A3o+Paulo%2C+1431+-+Vila+dos+Pinheiros%2C+Ibirit%C3%A9+-+MG%2C+32400-000!5e0!3m2!1spt-PT!2sbr!4v1529158228892" frameborder="0" style="border:0" allowfullscreen></iframe>
             <buttom class="button-white" type="submit">Ver no Google</buttom>
          </div>
     </section>
